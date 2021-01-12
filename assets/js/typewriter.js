8e2ac554e2c372c10528375234f958a88522d917
raewynd <raewynd@gmail.com>
2021-01-11 19:35:17 -0500

    website v2 by Alessandra

---
+++ assets/js/typewriter.js
@@ -0,0 +1,6 @@
+/* Typewriter */
+const instance = new Typewriter('#typewriter', {
+    strings: ['nano lunar rover', 'unmanned American lunar rover', 'student built lunar rover'],
+  	autoStart: true,
+  	loop: true,
+});
