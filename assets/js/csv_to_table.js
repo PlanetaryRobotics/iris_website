6ea4b7561bbb7c45ca92542a845faf188d493c47
Matt Davis <22134678+cmumatt@users.noreply.github.com>
2023-10-25 14:55:47 -0400

    fix: reflow team/donor table on mobile (#35)

--- assets/js/csv_to_table.js
+++ assets/js/csv_to_table.js
@@ -7,48 +7,32 @@ async function createTable(div_name, data_file) {
     const donation_data_table = data.split('\n').slice(1);
     const num_donors = donation_data_table.length;
 
-
     // creates a <table> element and a <tbody> element
-    const tbl = document.createElement("table");
-    const tblBody = document.createElement("tbody");
-    const cols = 4;
-    const rows = num_donors / cols;
-    console.log(num_donors, cols, rows)
+    const tbl = document.createElement("div");
+    tbl.classList.add("flex-container","container", "text-color-5", "p-style-small");
+    tbl.style = "border-top: black solid 1px; border-left: black solid 1px;";
+    console.log(num_donors);
 
     // creating all cells
-    for (let i = 0; i < rows; i++) {
-        // creates a table row
-        const row = document.createElement("tr");
+    for (let i = 0; i < num_donors; i++) {
+        // Outer cell
+        const cell = document.createElement("div");
+        cell.classList.add("three-columns");
+        cell.style="display: table; border-bottom: black solid 1px; border-right: black solid 1px;";
 
-        for (let j = 0; j < cols; j++) {
-        // Create a <td> element and a text node, make the text
-        // node the contents of the <td>, and put the <td> at
-        // the end of the table row
-        const cell = document.createElement("td");
-        const index = i*cols+j
-        console.log(index, i*cols+j < num_donors)
-        if (i*cols+j < num_donors){
-            const cellText = document.createTextNode(donation_data_table[index].split(',')[0]);
-            cell.appendChild(cellText);
-        }
-        else{
-            const cellText = document.createTextNode("")
-            cell.appendChild(cellText);
-        }
+        // Inner cell (for vertical alignment)
+        const cellInner = document.createElement("div");
+        cellInner.style = "display: table-cell; vertical-align: middle; text-align: center; padding: 5px;";
+        cell.appendChild(cellInner);
 
-        row.appendChild(cell);
-        }
+        // Text
+        const cellText = document.createTextNode(donation_data_table[i].split(',')[0]);
+        cellInner.appendChild(cellText);
 
-        // add the row to the end of the table body
-        tblBody.appendChild(row);
+        // Add the cell to the table
+        tbl.appendChild(cell);
     }
     
-    // put the <tbody> in the <table>
-        tbl.appendChild(tblBody);
-    // appends <table> into <body>
+    // appends <div> onto <body>
     document.getElementById(div_name).appendChild(tbl)
-    // document.body.appendChild(tbl);
-    // sets the border attribute of tbl to '2'
-    tbl.setAttribute("border", "0");
-
   }
